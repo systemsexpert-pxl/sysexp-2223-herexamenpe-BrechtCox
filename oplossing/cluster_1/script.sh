ba0f286d7b4a7ccbe614febc3dd86698ed45090d
BrechtCox <Brecht.cox@student.pxl.be>
2023-08-28 17:53:23 +0200

    Update script.sh

--- oplossing/cluster_1/script.sh
+++ oplossing/cluster_1/script.sh
@@ -4,15 +4,14 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl apply -f environments/PROD/application.yaml
 helm repo add elastic https://helm.elastic.co
 helm install elastic elastic/elasticsearch
-sleep 240
+sleep 300
 helm install metricbeat elastic/metricbeat
 helm install kibana elastic/kibana
-sleep 30
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
-sleep 20
+sleep 25
 kubectl apply -f secret_runner.yaml
 kubectl apply -f https://github.com/actions-runner-controller/actions-runner-controller/releases/download/v0.20.4/actions-runner-controller.yaml
-sleep 20
+sleep 35
 kubectl apply -f runner.yaml
 #Sleep a few minutes before next commands because sometimes slow download of docker images
 kubectl port-forward svc/argocd-server -n argocd 8090:443 > /dev/null 2>&1 &
@@ -22,3 +21,5 @@ echo $(kubectl get secret argocd-initial-admin-secret -n argocd -ojsonpath='{.da
 #kubectl get secrets elasticsearch-master-credentials -ojsonpath='{.data.password}'
 echo ElasticPassword:
 echo $(kubectl get secrets elasticsearch-master-credentials -ojsonpath='{.data.password}') | base64 --decode
+export PASSWORD=$(kubectl get secrets elasticsearch-master-credentials -ojsonpath='{.data.password}' | base64 --decode)
+curl -u elastic:$PASSWORD -X POST "http://localhost:5601/api/kibana/dashboards/import" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @export.json
